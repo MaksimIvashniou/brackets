ad671523466491d48fc15256630937e084e9c72c
MIvashniou <maskim.ivashniou@gmail.com>
2022-07-19 00:43:44 +0300

    feat: implement solution recursive

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const bracketsList = bracketsConfig.map(item => item.join(''));
+  return cutRecursive(str, bracketsList)
+}
+
+const cutRecursive = (strBase, bracketsList) => {
+  let strCopy = strBase;
+
+  for (let item of bracketsList) {
+    strCopy = strCopy.replace(item, '');
+  }
+
+  if (strCopy.length === 0) {
+    return true;
+  } else if (strBase === strCopy) {
+    return false;
+  } else {
+    return cutRecursive(strCopy, bracketsList);
+  }
 }
